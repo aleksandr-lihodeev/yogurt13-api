8119f71d91e704ca64e965b5b744bee262d530c7
Aleksandr Likhodeev <13thjavascript@gmail.com>
2023-12-25 17:54:06 +0600

    commit

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5055;
 const corsOptions = {
-    origin: process.env.FRONTEND_HOST,
-    optionsSuccessStatus: 200,
+  // origin: process.env.FRONTEND_HOST,
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
@@ -25,12 +26,11 @@ app.use("/crud", crudRoute);
 app.use("/profile", profileRoute);
 app.use("/verify", verifyRoute);
 
-
 mongoose
-    .connect(process.env.DATABASE_URL)
-    .then(() => console.log("Connect db success"))
-    .catch((err) => console.error("Error connecting to MongoDB", err));
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log("Connect db success"))
+  .catch((err) => console.error("Error connecting to MongoDB", err));
 
 app.listen(port, () => {
-    console.log("Server is running at localhost: " + port);
+  console.log("Server is running at localhost: " + port);
 });
